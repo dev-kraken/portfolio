b4654f9a117f8cad4640732d5ab332879d814c8a
Dev Kraken <soman@devkraken.com>
2024-06-26 20:16:14 +0400

    Kraken tag error fixed

--- components/kraken-3d-tag.tsx
+++ components/kraken-3d-tag.tsx
@@ -2,12 +2,7 @@
 import * as THREE from "three";
 import { useEffect, useRef, useState } from "react";
 import { Canvas, extend, useThree, useFrame } from "@react-three/fiber";
-import {
-  useGLTF,
-  useTexture,
-  Environment,
-  Lightformer,
-} from "@react-three/drei";
+import { useGLTF, useTexture } from "@react-three/drei";
 import {
   BallCollider,
   CuboidCollider,
@@ -34,16 +29,8 @@ export default function Kraken3dTag() {
 }
 
 function Band({ maxSpeed = 50, minSpeed = 10 }) {
-  const band = useRef(null);
-  const fixed = useRef(null);
-  const j1 = useRef(null);
-  const j2 = useRef(null);
-  const j3 = useRef(null);
-  const card = useRef(null);
-  const vec = new THREE.Vector3();
-  const ang = new THREE.Vector3();
-  const rot = new THREE.Vector3();
-  const dir = new THREE.Vector3();
+  const band = useRef(), fixed = useRef(), j1 = useRef(), j2 = useRef(), j3 = useRef(), card = useRef() // prettier-ignore
+  const vec = new THREE.Vector3(), ang = new THREE.Vector3(), rot = new THREE.Vector3(), dir = new THREE.Vector3() // prettier-ignore
   const segmentProps = {
     type: "dynamic",
     canSleep: true,
@@ -66,29 +53,30 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
   const [dragged, drag] = useState(false);
   const [hovered, hover] = useState(false);
 
-  useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1]);
-  useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1]);
-  useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1]);
-  useSphericalJoint(j3, card, [
-    [0, 0, 0],
-    [0, 1.45, 0],
-  ]);
+  // @ts-ignore
+  useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
+  // @ts-ignore
+  useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
+  // @ts-ignore
+  useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
+  // @ts-ignore
+  useSphericalJoint(j3, card, [[0, 0, 0], [0, 1.45, 0]]) // prettier-ignore
 
   useEffect(() => {
     if (hovered) {
       document.body.style.cursor = dragged ? "grabbing" : "grab";
-      return () => {
-        document.body.style.cursor = "auto";
-      };
+      return () => void (document.body.style.cursor = "auto");
     }
   }, [hovered, dragged]);
-  // @ts-ignore
+
   useFrame((state, delta) => {
     if (dragged) {
       vec.set(state.pointer.x, state.pointer.y, 0.5).unproject(state.camera);
       dir.copy(vec).sub(state.camera.position).normalize();
       vec.add(dir.multiplyScalar(state.camera.position.length()));
+      // @ts-ignore
       [card, j1, j2, j3, fixed].forEach((ref) => ref.current?.wakeUp());
+      // @ts-ignore
       card.current?.setNextKinematicTranslation({
         x: vec.x - dragged.x,
         y: vec.y - dragged.y,
@@ -96,48 +84,61 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
       });
     }
     if (fixed.current) {
+      // Fix most of the jitter when over pulling the card
       [j1, j2].forEach((ref) => {
+        // @ts-ignore
         if (!ref.current.lerped)
           ref.current.lerped = new THREE.Vector3().copy(
             ref.current.translation(),
           );
+        // @ts-ignore
         const clampedDistance = Math.max(
           0.1,
           Math.min(1, ref.current.lerped.distanceTo(ref.current.translation())),
         );
+        // @ts-ignore
         ref.current.lerped.lerp(
           ref.current.translation(),
           delta * (minSpeed + clampedDistance * (maxSpeed - minSpeed)),
         );
       });
+      // @ts-ignore
+      // Calculate catmul curve
       curve.points[0].copy(j3.current.translation());
-      curve.points[1].copy(j2.current.lerped);
-      curve.points[2].copy(j1.current.lerped);
-      curve.points[3].copy(fixed.current.translation());
+      // @ts-ignore
+      curve.points[1].copy(j2.current.lerped); // @ts-ignore
+      curve.points[2].copy(j1.current.lerped); // @ts-ignore
+      curve.points[3].copy(fixed.current.translation()); // @ts-ignore
       band.current.geometry.setPoints(curve.getPoints(32));
-      ang.copy(card.current.angvel());
-      rot.copy(card.current.rotation());
-      card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.25, z: ang.z });
+      // Tilt it back towards the screen
+      // @ts-ignore
+      ang.copy(card.current.angvel()); // @ts-ignore
+      rot.copy(card.current.rotation()); // @ts-ignore
+      card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.25, z: ang.z }); // @ts-ignore
     }
   });
 
   curve.curveType = "chordal";
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-
   // @ts-ignore
   return (
     <>
       <group position={[0, 4, 0]}>
+        {/*// @ts-ignore*/}
         <RigidBody ref={fixed} {...segmentProps} type="fixed" />
+        {/*// @ts-ignore*/}
         <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}>
           <BallCollider args={[0.1]} />
         </RigidBody>
+        {/*// @ts-ignore*/}
         <RigidBody position={[1, 0, 0]} ref={j2} {...segmentProps}>
           <BallCollider args={[0.1]} />
         </RigidBody>
+        {/*// @ts-ignore*/}
         <RigidBody position={[1.5, 0, 0]} ref={j3} {...segmentProps}>
           <BallCollider args={[0.1]} />
         </RigidBody>
+        {/*// @ts-ignore*/}
         <RigidBody
           position={[2, 0, 0]}
           ref={card}
@@ -150,9 +151,11 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
             position={[0, -1.2, -0.05]}
             onPointerOver={() => hover(true)}
             onPointerOut={() => hover(false)}
+            // @ts-ignore
             onPointerUp={(e) => (
               e.target.releasePointerCapture(e.pointerId), drag(false)
             )}
+            // @ts-ignore
             onPointerDown={(e) => (
               e.target.setPointerCapture(e.pointerId),
               drag(
@@ -162,7 +165,9 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
               )
             )}
           >
+            {/*// @ts-ignore*/}
             <mesh geometry={nodes.card.geometry}>
+              {/*// @ts-ignore*/}
               <meshPhysicalMaterial
                 map={materials.base.map}
                 map-anisotropy={16}
@@ -172,17 +177,22 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
                 metalness={0.5}
               />
             </mesh>
+            {/*// @ts-ignore*/}
             <mesh
               geometry={nodes.clip.geometry}
               material={materials.metal}
               material-roughness={0.3}
             />
+            {/*// @ts-ignore*/}
             <mesh geometry={nodes.clamp.geometry} material={materials.metal} />
           </group>
         </RigidBody>
       </group>
+      {/*// @ts-ignore*/}
       <mesh ref={band}>
+        {/*// @ts-ignore*/}
         <meshLineGeometry />
+        {/*// @ts-ignore*/}
         <meshLineMaterial
           color="white"
           depthTest={false}
